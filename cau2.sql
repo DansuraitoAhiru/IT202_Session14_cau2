6e1aa23a439058a238e069d505042c05f8c42d4f
DansuraitoAhiru <vqviet2007@gmail.com>
2026-05-11 20:05:21 +0700

    Update cau2.sql

--- cau2.sql
+++ cau2.sql
@@ -32,7 +32,7 @@ select * from Beds;
 
 -- Việc để một bệnh nhân lơ lửng không có giường như trên cũng vi phạm đặc tính Atomicity (hoặc thực hiện hết hoặc ko thực hiện j cả)
 -- nghĩa là vì đặc tính này, hệ thống bị gián đoạn giữa chừng khiến dữ liệu ko nhất quán giường cũ đã bị bỏ trống, nhưng giường mới chưa nhận bệnh nhân
--- nên tốt nhất là phải thêm rollback để khi kết nối bị ngắt vẫn có thể khôi phục trở lại trạng thái ban đầu
+-- nên trường hợp đang mô phỏng lỗi thì thêm rollback để khi kết nối bị ngắt vẫn có thể khôi phục trở lại trạng thái ban đầu
 
 -- Phần B: sửa
 drop procedure TransferBed;
